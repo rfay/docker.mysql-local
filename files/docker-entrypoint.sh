5debbf8b13e5b4e7d4cfa154620f02ca4a17edce
Randy Fay <randy@randyfay.com>
2017-12-22 20:52:41 -0700

    Start with a clean slate in /var/lib/mysql

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -1,63 +1,60 @@
 #!/bin/bash
-set -x
+# set -x
 set -eu
 set -o pipefail
 
-# If mariadb itself has not been initialized, initialize it.
-# This will be the case if we're running on an uninitialized /var/lib/mysql
+SOCKET=/var/tmp/mysql.sock
+
+# If mariadb has not been initialized, initialize it.
+# Then create our 'db', database, 'db' user, and permissions.
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mkdir -p /var/lib/mysql
 	chown -R mysql:mysql /var/lib/mysql
 
 	echo 'Initializing mysql'
 	mysql_install_db --datadir="/var/lib/mysql"
-	CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-	GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+	mysqld --skip-networking >/tmp/mysqld.out 2>&1 &
+	pid="$!"
 
-	echo 'Mariadb initialized'
-fi
-
-mysqld --skip-networking &
-pid="$!"
-
-for i in {60..0}; do
-	if mysqladmin ping -uroot --socket=/var/tmp/mysql.sock 2>/dev/null; then
-		break
+	# Wait for the server to respond to mysqladmin ping, or fail if it never does,
+	# or if the process dies.
+	for i in {60..0}; do
+		if mysqladmin ping -uroot --socket=$SOCKET 2>/dev/null; then
+			break
+		fi
+		# Test to make sure we got it started in the first place. kill -s 0 just tests to see if process exists.
+		if ! kill -s 0 $pid 2>/dev/null; then
+			echo "MariaDB initialization startup failed"
+			exit 3
+		fi
+		echo "MariaDB initialization startup process in progress... Try# $i"
+		sleep 1
+	done
+	if [ "$i" -eq 0 ]; then
+		echo 'MariaDB initialization startup process timed out.'
+		exit 1
 	fi
-	# Test to make sure we got it started in the first place. kill -s 0 just tests to see if process exists.
-	if ! kill -s 0 $pid 2>/dev/null; then
-		echo "mysqld local startup failed"
-		exit 3
-	fi
-	echo "MySQL local startup process in progress... Try# $i"
-	sleep 1
-done
-if [ "$i" -eq 0 ]; then
-	echo 'MySQL local startup process failed.'
-	exit 1
-fi
 
-# If no $MYSQL_DATABASE database exists, run initialization
-# Create the database and users we need.
-# Note that password is empty while we're running in socket mode.
-# The value in ~/.my.cnf has to be overridden here.
-if ! mysql -uroot -e "use $MYSQL_DATABASE;" 2>/dev/null; then
-	mysql mysql -uroot --password='' -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;"
+	mysql --database=mysql -uroot --password='' -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;"
 
 	mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -uroot --password='' mysql
 
 	mysql -uroot  --password='' -e "CREATE USER '"$MYSQL_USER"'@'%' IDENTIFIED BY '"$MYSQL_PASSWORD"' ;"
 	mysql -uroot  --password='' -e "GRANT ALL ON \`"$MYSQL_DATABASE"\`.* TO '"$MYSQL_USER"'@'%' ;"
 	mysql -uroot  --password='' -e 'FLUSH PRIVILEGES ;'
-fi
 
-if ! kill -s TERM "$pid" || ! wait "$pid"; then
-	echo >&2 'MySQL local startup process failed.'
-	exit 1
+	mysqladmin --socket=$SOCKET  -uroot password "$MYSQL_ROOT_PASSWORD"
+
+	if ! kill -s TERM "$pid" || ! wait "$pid"; then
+		echo >&2 'Mariadb initialization startup process failed.'
+		exit 1
+	fi
+
+	echo 'Mariadb initialized'
 fi
 
 echo
-echo 'MySQL local startup process done. Ready for start up.'
+echo 'MySQL startup process done. Ready for server start up.'
 echo
 
 
