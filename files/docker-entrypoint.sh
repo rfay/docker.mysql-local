830e135fd1a89de7bc9be50f71b31bc63fd5ffc2
Randy Fay <randy@randyfay.com>
2017-12-22 17:36:14 -0700

    Better docker-entrypoint.sh so that we initialize whether or not there's a mount point

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -3,12 +3,25 @@ set -x
 set -eu
 set -o pipefail
 
+# If mariadb itself has not been initialized, initialize it.
+# This will be the case if we're running on an uninitialized /var/lib/mysql
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	mkdir -p /var/lib/mysql
+	chown -R mysql:mysql /var/lib/mysql
+
+	echo 'Initializing mysql'
+	mysql_install_db --datadir="/var/lib/mysql"
+	CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+	GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+
+	echo 'Mariadb initialized'
+fi
 
 mysqld --skip-networking &
 pid="$!"
 
 for i in {60..0}; do
-	if mysqladmin ping -uroot -p$MYSQL_ROOT_PASSWORD --socket=/var/tmp/mysql.sock 2>/dev/null; then
+	if mysqladmin ping -uroot --socket=/var/tmp/mysql.sock 2>/dev/null; then
 		break
 	fi
 	# Test to make sure we got it started in the first place. kill -s 0 just tests to see if process exists.
@@ -24,18 +37,18 @@ if [ "$i" -eq 0 ]; then
 	exit 1
 fi
 
-# If no mysql database exists, run initialization
+# If no $MYSQL_DATABASE database exists, run initialization
 # Create the database and users we need.
-if ! mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "use $MYSQL_DATABASE;" 2>/dev/null; then
-	mysql mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;"
+# Note that password is empty while we're running in socket mode.
+# The value in ~/.my.cnf has to be overridden here.
+if ! mysql -uroot -e "use $MYSQL_DATABASE;" 2>/dev/null; then
+	mysql mysql -uroot --password='' -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;"
 
-	mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -uroot -p$MYSQL_ROOT_PASSWORD mysql
+	mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -uroot --password='' mysql
 
-	mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '"$MYSQL_USER"'@'%' IDENTIFIED BY '"$MYSQL_PASSWORD"' ;"
-	mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON \`"$MYSQL_DATABASE"\`.* TO '"$MYSQL_USER"'@'%' ;"
-	mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e 'FLUSH PRIVILEGES ;'
-
-
+	mysql -uroot  --password='' -e "CREATE USER '"$MYSQL_USER"'@'%' IDENTIFIED BY '"$MYSQL_PASSWORD"' ;"
+	mysql -uroot  --password='' -e "GRANT ALL ON \`"$MYSQL_DATABASE"\`.* TO '"$MYSQL_USER"'@'%' ;"
+	mysql -uroot  --password='' -e 'FLUSH PRIVILEGES ;'
 fi
 
 if ! kill -s TERM "$pid" || ! wait "$pid"; then
